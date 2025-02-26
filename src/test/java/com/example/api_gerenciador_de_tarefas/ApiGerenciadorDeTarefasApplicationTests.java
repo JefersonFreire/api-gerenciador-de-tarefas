c0d886b71ba22980ec42ad67aba8e6525183921c
JefersonFreire <jfreire86@hotmail.com>
2025-02-25 21:27:41 -0300

    configurações iniciais

---
+++ src/test/java/com/example/api_gerenciador_de_tarefas/ApiGerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_gerenciador_de_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
